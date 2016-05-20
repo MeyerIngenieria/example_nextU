875b0803d6f2ea5fb8162d84027c18ef3696cd57
Daniel Meyer <damfe2005@gmail.com>
2016-05-19 23:11:05 -0300

    version 18

--- Porton/ViewController.swift
+++ Porton/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MessageLabel: UILabel!
+    @IBOutlet weak var nametextfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func ChangeMessage(sender: AnyObject) {
+        MessageLabel.text = "Hola  \(nametextfield.text)"
+        view.endEditing(true)
+        
+    }
+// comentarios
+    
 }
 
